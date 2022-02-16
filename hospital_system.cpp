c18a5a153310240b5a00c70ec1cbf193010f41f3
SaadMu7ammad <74321306+SaadMu7ammad@users.noreply.github.com>
2022-02-16 22:35:20 +0200

    project_1
    Implement the following system for a hospital
    ● There are 20 different specialization (e.g. Children, Surgery, etc)
    ● For each specialization, there are only 5 available spots [queue]
    ● Adding a patient
    ○ Read the requested specialization [1-20].
    ○ Read his name and status (0 = regular, 1 urgent)
    ○ If 5 patients exist, apologize and don’t accept.
    ○ If the user is regular, add in end of queue. Otherwise, add in Begin
    ● Print patients, for the specializations that have waiting patients 
    ● Dr pickup a patient
    ○ Read the requested specialization. If no patients, inform the doctor
    ○ Otherwise, ask the patient to go with the Dr. Remove from the queue

---
+++ hospital_system.cpp
@@ -0,0 +1,217 @@
+#include <iostream>
+#include <algorithm>
+#include<bits/stdc++.h>
+#define loop(n) for(int i = 0; i < n; i++)
+
+typedef long long ll;
+
+using namespace std;
+void fast()
+{
+
+
+    cin.tie(0);
+    cin.sync_with_stdio(0);
+}
+
+const int mxspec=20;
+const int mx=5+1;
+string names[mxspec][mx];
+int Queue[mxspec] ; //such as frequency array to detect which specialization more than 5
+int status[mxspec][mx] ; //we added +1 although the tot num is 20*5 to make a one extra space when we shift the whole array
+// will see next
+int tracker=0;
+
+
+
+
+int menu()
+{
+    int choice =0;
+
+    while(choice==0)
+    {
+
+        cout << "\nEnter your choice:\n";
+        cout << "1) Add new patient\n";
+        cout << "2) Print all patients\n";
+        cout << "3) Get next patient\n";
+        cout << "4) Exit\n";
+
+        cin >> choice;
+
+        if (choice>4 || choice<1)
+        {
+            cout << "Invalid choice. Try again\n";
+            choice = 0;	// loop keep working
+        }
+
+
+
+
+    }
+
+    return choice;
+
+
+}
+void shift_to_left(int specs,int mxqueue)
+{
+
+    for(int i =1; i<=mxqueue; i++)
+    {
+
+
+        names[specs][i-1]=names[specs][i];
+        status[specs][i-1]=status[specs][i];
+
+    }
+
+
+
+}
+void shift_to_right(int specs,int mxqueue)
+{
+
+
+
+    for(int i =mxqueue; i > 0; i--)
+    {
+
+
+        names[specs][i]=names[specs][i-1];
+        status[specs][i]=status[specs][i-1];
+
+    }
+
+
+
+
+
+
+
+}
+
+void system()
+{
+
+
+
+
+
+    while(true)
+    {
+        int choice=menu();
+        if(choice==1)
+        {
+            int spec,stat;
+            string name;
+            cout<< "Enter specialization, name, status: ";
+
+            cin>>spec>>name>>stat;
+
+
+
+            if(Queue[spec]==5)
+            {
+                //Queue[spec]--;
+
+                cout << "Sorry we can't add more patients for this specialization\n";
+
+            }
+            else if(stat==1)  //urgent so it must put first so we shift the whole array
+            {
+
+                shift_to_right(spec,Queue[spec]);
+                Queue[spec]++;
+                names[spec][0]=name;
+                status[spec][0]=stat;
+                // ++tracker;
+
+            }
+            else if(stat==0)  //regular
+            {
+
+                int position=Queue[spec];
+
+
+                names[spec][position]=name;
+                status[spec][position]=stat;
+                Queue[spec]++;
+                // ++tracker;
+            }
+
+
+        }
+
+        else if(choice==2)
+        {
+
+
+            for(int i=0; i<mxspec; i++)
+            {
+                if(Queue[i])
+                {
+                    cout<<"There are "<<Queue[i]<<" patients in specialization "<<i<<"\n";
+
+                    for(int j=0; j<Queue[i]; j++)
+                    {
+
+                        // int counter=Queue[i];
+
+                        cout<<names[i][j]<<" "<<status[i][j]<<"\n";
+
+
+                    }
+                }
+            }
+        }
+        else if (choice==3)
+        {
+            cout<<"Enter Specialization num: ";
+            int specialNum;
+            cin>>specialNum;
+            if(Queue[specialNum]==0)
+            {
+
+
+
+                cout<<"No patients at the moment , Have a rest :) \n";
+            }
+            else
+            {
+                Queue[specialNum]--;
+                cout<<names[specialNum][0]<<" GO with the DR ,please\n";
+                shift_to_left( specialNum,Queue[specialNum]);
+
+
+            }
+
+
+
+
+        }
+        else if (choice==4)
+        {
+
+            cout<<" THE PROGRAM CLOSED \n";
+            break;
+
+        }
+
+
+    }
+
+
+}
+
+
+
+
+int main()
+{
+
+    system();
+
+    return 0;
+}
