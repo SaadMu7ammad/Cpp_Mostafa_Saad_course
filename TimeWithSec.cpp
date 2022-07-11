bb22e2def47aef18cfd22de96e115899fc2f1c5d
SaadMu7ammad <74321306+SaadMu7ammad@users.noreply.github.com>
2022-07-11 09:08:13 +0200

    258.Class Constructor & Destructor Homework7
    dealing with getters and setters

---
+++ TimeWithSec.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+class Time {
+private:
+	//int hours, minutes, seconds; these members replaced with only tot (total seconds)
+	int tot;
+public:
+	Time(int tot) :tot(tot) {}
+	int GetTotalSeconds() {
+		return tot;
+	}
+	int GetTotalMinutes() {
+		return tot / 60;
+	}
+	void PrintHHMMSS() {
+		cout << GetHours() << ":" << GetMinutes() << ":" << GetSeconds() << "\n";
+	}
+	string ToSring(string seperator = "-") {
+		ostringstream oss;
+		oss << GetHours() << seperator << GetMinutes() << seperator << GetSeconds();
+		return oss.str();
+	}
+	int GetHours() {
+		return tot/3600;
+	}
+	int GetMinutes() {
+		return (tot-(GetHours()*3600))/60;
+	}
+	int GetSeconds() {
+		return (tot - ((GetMinutes()*60) +(GetHours()* 3600)));
+	}
+};
+int main() {
+	Time t(4000);
+	t.PrintHHMMSS();
+	cout << "--" << endl;
+	  t= Time(4001);
+	t.PrintHHMMSS();
+	cout<<t.ToSring("//");
+	return 0;
+}
